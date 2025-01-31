60fbefc884f322b5b414d3d123e6b8b27d0c9211
Austin Moody <amoody@ainq.com>
2025-01-31 16:19:17 -0500

    Updates to NDMS bundle processing.

--- ndms/src/main/java/com/lantanagroup/link/ndms/NdmsMeasureReportGenerator.java
+++ ndms/src/main/java/com/lantanagroup/link/ndms/NdmsMeasureReportGenerator.java
@@ -7,7 +7,6 @@ import com.lantanagroup.link.config.api.ApiConfig;
 import com.lantanagroup.link.model.ReportContext;
 import com.lantanagroup.link.model.ReportCriteria;
 import org.apache.commons.lang3.StringUtils;
-import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r4.model.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,9 +16,6 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.ParseException;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
@@ -51,7 +47,6 @@ public class NdmsMeasureReportGenerator implements IMeasureReportGenerator {
             final Date endDate = Helper.parseFhirDate(criteria.getPeriodEnd());
 
             // Read in NHSN bed list
-            // TODO: Will revisit this is a bit of a mess to get initial Measures for comparison
             String nhsnCsvData = Files.readString(Path.of(config.getNhsnBedListCsvFile()));
             final List<NhsnLocation> nhsnLocations = NhsnLocation.parseCsvData(nhsnCsvData);
 
@@ -74,56 +69,18 @@ public class NdmsMeasureReportGenerator implements IMeasureReportGenerator {
                                     .map(Bundle.BundleEntryComponent::getResource)
                                     .filter(Encounter.class::isInstance)
                                     .map(Encounter.class::cast)
-                                    .filter(encounter -> isEncounterInRange(encounter, startDate, endDate, patientDataBundleId))
+                                    .filter(encounter -> isEncounterInRange(encounter, startDate, endDate))
                                     .collect(Collectors.toList());
 
                             // Loop encounters and pull associated Location options
-                            List<Location> relevantLocations = new ArrayList<>();
+                            List<Location> allEncounterLocations = new ArrayList<>();
                             encounters.forEach(encounter -> {
                                 List<Location> locs = getLocationsForEncounter(encounter, patientBundle);
-                                relevantLocations.addAll(locs);
+                                allEncounterLocations.addAll(locs);
                             });
-
-                            // Pull Location identifiers from Encounters if the Location has a period.start / period.end
-                            // that falls in range of the passed in start/end dates when generating the reports.
-                            // TODO add relevat location check onto EncounterLocationComponent after Encounter
-                            // has been identified to contain a valid Location via date
-                            // I think we are pulling locations in that DO NOT have a date.  Becauce we are pulling the
-                            // encounter that has at least 1 relevant Locaiton with a date.  And then pulling all those locations.
-                            List<String> relevantLocationIdentifiers = patientBundle.getEntry().stream()
-                                    .map(Bundle.BundleEntryComponent::getResource)
-                                    .filter(Encounter.class::isInstance)
-                                    .map(Encounter.class::cast)
-                                    .filter(encounter -> hasRelevantLocation(encounter, startDate, endDate, patientDataBundleId))
-                                    .flatMap(encounter -> encounter.getLocation().stream())
-                                    .map(location -> location.getLocation().getReference())
-                                    .filter(Objects::nonNull)  // remove nulls
-                                    .filter(ref -> !ref.isEmpty())  // remove empty strings
+                            // De-dupe locations
+                            List<Location> relevantLocations = allEncounterLocations.stream()
                                     .distinct()
-                                    .collect(Collectors.toList());
-
-                            List<String> relevantLocationIdentifiersNOTGOOD = patientBundle.getEntry().stream()
-                                    .map(Bundle.BundleEntryComponent::getResource)
-                                    .filter(Encounter.class::isInstance)
-                                    .map(Encounter.class::cast)
-                                    .flatMap(encounter -> encounter.getLocation().stream())
-                                    // Now we filter the locations themselves using isLocationRelevantOrig
-                                    .filter(location -> isLocationRelevantOrig(location, startDate, endDate, patientDataBundleId))
-                                    .map(location -> location.getLocation().getReference())
-                                    .filter(Objects::nonNull)  // remove nulls
-                                    .filter(ref -> !ref.isEmpty())  // remove empty strings
-                                    .distinct()
-                                    .collect(Collectors.toList());
-
-                            // Pull relevant Location resources using the list if ID's generated in previous step.
-                            // Filter out any Location that has no aliases as those are used for finding the Nebraska
-                            // Med bed code.
-                            List<Location> relevantLocationsOLD = patientBundle.getEntry().stream()
-                                    .map(Bundle.BundleEntryComponent::getResource)
-                                    .filter(Location.class::isInstance)
-                                    .map(Location.class::cast)
-                                    .filter(location -> relevantLocationIdentifiers.contains("Location/" + location.getIdElement().getIdPart()))
-                                    .filter(location -> location.hasAlias() && !location.getAlias().isEmpty())
                                     .collect(Collectors.toList());
 
                             for (Location location : relevantLocations) {
@@ -139,39 +96,11 @@ public class NdmsMeasureReportGenerator implements IMeasureReportGenerator {
                                 }
 
                                 if (!aliases.isEmpty()) {
-
                                     Optional<NhsnLocation> nhsnLocation = getNhsnLocation(nhsnLocations, aliases);
-                                    if (!nhsnLocation.isPresent()) {
-
-                                        // We want to log the location to go back and research
-                                        logger.info("UNMAPPED|{}|{}|{}|{}|{}|{}|{}",
-                                                patientDataBundleId,
-                                                location.getId(),
-                                                location.getName(),
-                                                location.getAlias().stream().map(StringType::toString).collect(Collectors.joining(",")),
-                                                location.getPartOf().getDisplay(),
-                                                Optional.ofNullable(partOf).map(p -> p.getId()).orElse(""),
-                                                Optional.ofNullable(partOf).map(p -> p.getAlias().stream().map(StringType::toString).collect(Collectors.joining(","))).orElse("")
-                                        );
-                                    } else {
-                                        NhsnLocation foundLocation = nhsnLocation.get();
-                                        logger.info("ISMAPPED|{}|{}|{}|{}|{}|{}|{}|{}",
-                                                foundLocation.getOrganizationId(),
-                                                foundLocation.getCode(),
-                                                foundLocation.getUnit(),
-                                                foundLocation.getCdcCode(),
-                                                foundLocation.getLocationCode(),
-                                                foundLocation.getCdcLabel(),
-                                                foundLocation.getStatus(),
-                                                foundLocation.getTrac2es()
-                                        );
-                                    }
                                     nhsnLocation.ifPresent(
                                     loc -> {
 
                                                 // Lookup TRAC2ES Code
-                                                // TODO: !!! Right now if we do not have a NebMed to TRAC2ES Map then we do not include
-                                                //       Looking for feedback from NDMS re: this.  So this may change.
                                                 CodeableConcept groupCodeableConcept = getTrac2esCodeableConcept(config.getEvaluationService(), config.getTrac2esCodeSystem(), loc.getTrac2es());
 
                                                 if (groupCodeableConcept != null) {
@@ -187,8 +116,6 @@ public class NdmsMeasureReportGenerator implements IMeasureReportGenerator {
 
                                                     patientMeasureReport.addGroup(group);
 
-                                                    //logger.info("Patient Bundle Mapped: {}", patientDataBundleId);
-
                                                 }
                                             }
                                     );
@@ -201,7 +128,8 @@ public class NdmsMeasureReportGenerator implements IMeasureReportGenerator {
 
                         String measureReportId = ReportIdHelper.getPatientMeasureReportId(measureContext.getReportId(), patient.getId());
                         patientMeasureReport.setId(measureReportId);
-                        // Tag individual MeasureReport as patient-data as it references a patient and will be found for expunge
+                        // Tag individual MeasureReport as patient-data as it references a patient and
+                        // will be found during Expunge API calls
                         patientMeasureReport.getMeta().addTag(Constants.MAIN_SYSTEM, Constants.PATIENT_DATA_TAG,"");
 
                         logger.info("Persisting patient {} measure report with id {}", patient, measureReportId);
@@ -214,7 +142,8 @@ public class NdmsMeasureReportGenerator implements IMeasureReportGenerator {
 
                         return patientMeasureReport;
                     }).collect(Collectors.toList())).get();
-            // to avoid thread collision remove saving the patientMeasureReport on the FhirServer from the above parallelStream
+            // To avoid thread collision, we remove saving the patientMeasureReport
+            // on the FhirServer from the above parallelStream
             // pass them to aggregators using measureContext
             measureContext.setPatientReports(patientMeasureReports);
         } finally {
@@ -224,7 +153,7 @@ public class NdmsMeasureReportGenerator implements IMeasureReportGenerator {
         }
         MeasureReport masterMeasureReport = reportAggregator.generate(criteria, reportContext, measureContext, config);
 
-        // Add  Organization Information to MeasureReport
+        // Add Organization Information to MeasureReport
         ndmsUtility.addLocationSubjectToMeasureReport(masterMeasureReport, reportContext.getReportLocation());
 
         measureContext.setMeasureReport(masterMeasureReport);
@@ -249,14 +178,7 @@ public class NdmsMeasureReportGenerator implements IMeasureReportGenerator {
 
     }
 
-    private static boolean hasRelevantLocation(Encounter encounter, Date startDate, Date endDate, String patientBundleId) {
-        boolean returnValue = encounter.getLocation().stream()
-                .anyMatch(location -> isLocationRelevantOrig(location, startDate, endDate, patientBundleId));
-
-        return returnValue;
-    }
-
-    private static boolean isEncounterInRange(Encounter encounter, Date startDate, Date endDate, String patientBundleId) {
+    private static boolean isEncounterInRange(Encounter encounter, Date startDate, Date endDate) {
         if (encounter.getPeriod() == null) {
             return false;
         }
@@ -268,53 +190,11 @@ public class NdmsMeasureReportGenerator implements IMeasureReportGenerator {
             return false;
         }
 
-        boolean returnValue =  (encounterStart == null || !encounterStart.after(startDate)) &&
+        return (encounterStart == null || !encounterStart.after(startDate)) &&
                 (encounterEnd == null || !encounterEnd.before(endDate));
-
-        return returnValue;
-
-
-    }
-
-    private static boolean isLocationRelevantOrig(Encounter.EncounterLocationComponent location, Date startDate, Date endDate, String patientBundleId) {
-
-        if (location.getPeriod() == null) {
-            return false; // If no period is specified, consider it NOT relevant
-        }
-
-        Date locationStart = location.getPeriod().getStart();
-        Date locationEnd = location.getPeriod().getEnd();
-
-        // Period exists, but both start/stop don't exist consider NOT relevant
-        if (locationStart == null && locationEnd == null) {
-            return false;
-        }
-
-        boolean returnValue =  (locationStart == null || !locationStart.after(startDate)) &&
-                (locationEnd == null || !locationEnd.before(endDate));
-
-        return returnValue;
     }
 
     private Optional<NhsnLocation> getNhsnLocation(List<NhsnLocation> nhsnLocations, List<String> aliases) {
-
-        // TODO: Need to verify this mapping w/ NDMS
-        // At first I was looking first for one of the aliases in the "Unit Label" column.
-        // Then for those returned columns further looking for one of the aliases in the
-        // "Your Code" column.  Digging around in Bellevue data on 20-Oct-2024 it almost
-        // appears that it could be an either or situation?  So the commented out code
-        // below is from pre 20-October-2024.  Now going to try either Unit Label or
-        // Your Code.
-//        List<NhsnLocation> locationsByUnitLabel = nhsnLocations.stream()
-//                .filter(location -> aliases.contains(location.getUnit()))
-//                .collect(Collectors.toList());
-//
-//        if (!locationsByUnitLabel.isEmpty()) {
-//            return locationsByUnitLabel.stream()
-//                    .filter(location -> aliases.contains(location.getCode()))
-//                    .findFirst();
-//        }
-
 
         List<NhsnLocation> byUnit = nhsnLocations.stream()
                 .filter(
@@ -339,11 +219,9 @@ public class NdmsMeasureReportGenerator implements IMeasureReportGenerator {
 
     private CodeableConcept getTrac2esCodeableConcept(String evaluationServiceLocation, String codeSystemLocation, String trac2esCode) {
 
-        // Here we take the TRAC2ES code which we got from looking up the NDMS/BEL code
-        // And lookup the full Coding information from the configured TRAC2ES CodeSystem
-        // which we are going to assume is loaded on the CQF Evaluation server
-        // REFACTOR
-
+        // Here we take the TRAC2ES code which we got from looking up the NDMS/BEL code.
+        // Then look up the full Coding information from the configured TRAC2ES CodeSystem.
+        // Which we are going to assume is loaded on the CQF Evaluation server
         // TODO - need a default "no map" concept.
         CodeableConcept codeableConcept = null;
 
